a1edaac28e399ceb2647b594b9fcfb4960f414e6
Crowdbotics <team@crowdbotics.com>
2022-01-13 16:16:19 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,12 @@
+from django.conf import settings
 from django.db import models
+
+
+class Account(models.Model):
+    "Generated Model"
+    acc_no = models.BigIntegerField()
+    account_name = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
